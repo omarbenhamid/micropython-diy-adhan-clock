23adb689e80994db56d728cf47aa444296df2c16
Omar BENHAMID <contact@obenhamid.me>
2019-09-24 20:30:38 +0200

    WIP Changed salat db : indexing by minute of year

--- src/timesdb.py
+++ src/timesdb.py
@@ -30,6 +30,13 @@ def nextday(self, year, month, day):
     
     return (year, month, day)
 
+def minuteofyear(month,day, h, m):
+    d = 0
+    for i in range(1, month):
+        d += dom(i)
+    d += day - 1
+    return (d * 24 + h) * 60 + m
+
 ### Salat Times Database
         
 class SalatDB:
@@ -37,32 +44,46 @@ class SalatDB:
         try:
             self.f = open(dbfile, "r+b")
         except OSError:
-            debug("Salat db '",dbfile,"' seems not to exist ? creating a new one")
+            print("Salat db '",dbfile,"' seems not to exist ? creating a new one")
             self.f = open(dbfile, "w+b")
         self.db = btree.open(self.f)
     
+    def isempty(self):
+        try:
+            next(self.db.keys())
+            return False
+        except StopIteration:
+            return True
+        
+    def save(self):
+        self.db.flush()
     
     def findfirstsalatafter(self, year, month, day, hour, min, failifnotfound=False):
         """ Will return info for next salat a tuple : (salatindex,time) 
             time is in sceonds since epoch"""
-        for key, stime in self.db.items("%02d-%02d" % (month,day)):
-            h,m = (int(x) for x in stime.split(b":"))
-            if h < hour: continue #Bad guess
-            if h == hour and m <= min: continue #Bad guess
-            mo,da,sidx = (int(x) for x in key.split(b'-'))
+        moy = minuteofyear(month, day, hour, min)
+        bisextile = year % 4 == 0
+        
+        for nextsalat in self.db.values("%06d" % moy):
+            sidx,mo,da,h,m = (int(x) for x in nextsalat.split(b','))
+            if mo == 2 and da == 29 and not bisextile: continue
             while h >= 24:
                 h = h - 24
                 year, mo, da = nextday(year, mo, da)
-            return (sidx, time.mktime((year, mo, da, h, m, 0, -1, -1)))
+            return (sidx, (year, mo, da, h, m))
         
-        if failifnotfound: raise Exception("Salat data seems missing.")
-        return self.findfirstsalatafter(year+1,1,1,-1,-1, True)
+        # IF we get here means no data found for current yer :
+        if failifnotfound: raise Exception("Salat data missing.")
+        return self.findfirstsalatafter(year+1,1,1,0,0, True)
     
-    def findnextsalat(self):
+    def findnextsalat(self, mindelayminutes=0):
         """ REturn the next salat after 'now' """
+        
         y,m,d,h,mi,_,_,_ = time.localtime()
-        return self.findfirstsalatafter(y, m, d, h, mi)
-
+        mi = mi + +mindelayminutes
+        
+        sidx, (oy, omo, oda, oh, om) = self.findfirstsalatafter(y, m, d, h, mi)
+        return sidx, time.mktime((oy, omo, oda, oh, om,0,-1,-1))
     
     def setstime(self, month, day, sidx, stime):
         """ Update salat with given time 
@@ -79,7 +100,7 @@ class SalatDB:
         except ValueError:
             raise ValueError("Bad time : %s" % stime)
         
-        self.db["%02d-%02d-%02d" % (month, day, sidx)]=stime.strip()
+        self.db["%06d" % minuteofyear(month, day, h, m)]="%d,%d,%d,%d,%d" % (sidx, month, day, h, m)
     
     def importcsv(self, csvlines):
         """Example : MM,DD,FH:FM,CH:CM,DH:DM,AH:AM,MH:MM,IH:IM"""
@@ -104,6 +125,7 @@ class SalatDB:
                     self.setstime(m,d,idx,stime)
             except ValueError as err:
                 raise ValueError("Line %d : bad times ... : %s" % str(err))
+        self.save()
             
     def close(self):
         self.db.close()
