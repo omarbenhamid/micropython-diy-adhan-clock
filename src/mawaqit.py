d399c7a23528e03d2ce3936daaff542afd046a8c
Omar BENHAMID <contact@obenhamid.me>
2020-03-16 19:14:41 +0100

    WIP adding mawaqit support

---
+++ src/mawaqit.py
@@ -0,0 +1,57 @@
+import time
+import json
+import network
+import urequests
+import naya
+import io
+import gc
+
+WIFI_CONN_TIMEOUT_MS=30*1000
+
+conn = network.WLAN(network.STA_IF)
+
+def _iter_months(token_stream):
+    for t,n in token_stream:
+        if n != "calendar": continue
+        t,n = next(token_stream)
+        if n != ':': continue
+        break
+    assert(next(token_stream)==(0,'['))
+    tok = (0,',')
+    mnum = 0
+    while tok == (0,','):
+        mnum = mnum + 1
+        month, tok = naya.parse_next(token_stream)
+        yield (mnum, month)
+    
+def downloadtimes(SSID, password, apikey, mcode, sdb):
+    if not conn.isconnected() or not conn.active():
+        conn.active(True)
+        conn.disconnect()    
+        conn.connect(SSID,password)
+        s=time.ticks_ms()
+        while not conn.isconnected() and ((time.ticks_ms() - s) < WIFI_CONN_TIMEOUT_MS):
+            time.sleep_ms(500)
+        
+    if not conn.isconnected():
+        raise Exception("Cannot connect to wifi")
+
+    try:
+        r=urequests.get('https://mawaqit.net/api/2.0/mosque/%s/prayer-times?calendar=yes' % mcode,
+                  headers={"accept": "application/json","Api-Access-Token":apikey})
+        
+        if sdb == None: return r.content
+        for month, data in _iter_months(naya.tokenize(r.raw)):
+            print("Importing month", month)
+            print(data)
+            gc.collect()
+            sdb.import_mawaqit_month(month, data)
+        return None
+    finally:
+        conn.disconnect()
+
+def dosync(sdb):
+    with open("mawaqit.json","r") as f:
+        data = json.load(f)
+    return downloadtimes(sdb=sdb,**data)
+
---
+++ src/naya/__init__.py
@@ -0,0 +1,2 @@
+from naya.json import parse, parse_string, stream_array, tokenize, TOKEN_TYPE, parse_next
+__all__ = ["parse", "parse_string", "stream_array", "tokenize", "TOKEN_TYPE", "parse_next"]
